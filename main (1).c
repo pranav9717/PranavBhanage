a35e663987ab9d8e7927dd3ce624e5962aa34d6f
pranav9717 <45939886+pranav9717@users.noreply.github.com>
2019-10-30 15:15:55 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int n = 100;
+  printf ("First hundred number");
+  for (int i = 0; i < 100; i++)
+    {
+      printf ("%d\n", i);
+    }
+
+  return 0;
+}
